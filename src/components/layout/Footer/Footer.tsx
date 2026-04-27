2c0fbe5a8ca49177ce8c7f6f8cd24cee549673d9
Thomas Marguenot <thomas@agence-belle-epoque.fr>
2026-04-27 22:51:39 +0200

    feat: improve contact page for mobile

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 
 import { useIsMobile } from '@/hooks/useIsMobile';
+
 import type { FooterProps } from './Footer.types';
 import './Footer.css';
 
@@ -17,7 +18,9 @@ export function Footer({
         initial={isMobile ? { height: '100%' } : { height: '0%' }}
         whileInView={isMobile ? { height: '100%' } : { height: '100%' }}
         viewport={{ once: true }}
-        transition={isMobile ? { duration: 0 } : { duration: 1.2, ease: 'easeInOut' }}
+        transition={
+          isMobile ? { duration: 0 } : { duration: 1.2, ease: 'easeInOut' }
+        }
       />
       <div className="footer-container">
         <div className="footer-content">
@@ -26,7 +29,11 @@ export function Footer({
             initial={isMobile ? { x: 0, opacity: 1 } : { x: 370, opacity: 0 }}
             whileInView={isMobile ? { x: 0, opacity: 1 } : { x: 0, opacity: 1 }}
             viewport={{ once: true }}
-            transition={isMobile ? { duration: 0 } : { duration: 0.9, delay: 1.5, ease: 'easeOut' }}
+            transition={
+              isMobile
+                ? { duration: 0 }
+                : { duration: 0.9, delay: 1.5, ease: 'easeOut' }
+            }
           >
             {`© ${year} ${companyName}.`}
           </motion.p>
